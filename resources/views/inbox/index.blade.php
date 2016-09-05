773adad75174a100c2a5e36f8a55f9e74f3c8bbc
Bagas Udi Sahsangka <udibagas@gmail.com>
2016-09-06 06:35:10 +0700

    form inbox

--- resources/views/inbox/index.blade.php
+++ resources/views/inbox/index.blade.php
@@ -17,16 +17,17 @@
             </div>
         </div>
         <div class="col-md-5">
-            <p>
-                Untuk saran dan pertanyaan silakan hubungi kontak berikut atau isi form di bawah ini.
-            </p>
-
-            <div class="well text-bold">
-                Email : <a href="mailto:lontar.aditya@mail.com">lontar.aditya@mail.com</a><br>
-                WA/SMS : 08568648757
-            </div>
-
             <div class="well">
+                <p>
+                    Untuk saran dan pertanyaan silakan hubungi kontak berikut atau isi form di bawah ini.
+                </p>
+                <div class="text-bold">
+                    Email : <a href="mailto:lontar.aditya@mail.com">lontar.aditya@mail.com</a><br>
+                    WA/SMS : 08568648757
+                </div>
+
+                <br>
+
                 @include('inbox._form', ['url' => '/inbox', 'method' => 'POST'])
             </div>
         </div>
